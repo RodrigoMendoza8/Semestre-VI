7aa47acc1b7115fb126e8434defff6f514a48ea3
RodrigoMendoza8 <rodrigomendozardgz824@gmail.com>
2026-01-18 12:18:28 -0600

    Actividad 1

--- Algoritmos para Econometria/regresionLineal_Actividad1.py
+++ Algoritmos para Econometria/regresionLineal_Actividad1.py
@@ -27,7 +27,11 @@ b = promedio_y - (m * promedio_x)
 
 print(f"La ecuación del modelo es: y = {b:.2f} + {m:.2f}x ")
 print(f"Para un ingreso de 50, el consumo va a ser de: {predecir(50):.2f}")
-print(f"Error para el primer dato (y = {consumo[0]}) (y gorro = {predecir(ingresos[0]):.2f}): {error(consumo[0], ingresos[0]):.2f}")
+print(f"Error para X = 10 (y = {consumo[3]}) (y gorro = {predecir(ingresos[3]):.2f}): {error(consumo[3], ingresos[3]):.2f}")
+
+'''
+Al calcular el error para X = 10, obtenemos un valor de 0.04, lo que indica que el modelo predice el consumo con bastante precisión.
+'''
 
 plt.figure()
 plt.scatter(ingresos, consumo, label='Datos')
@@ -39,3 +43,8 @@ plt.ylabel('Consumo')
 plt.legend()
 plt.grid()
 plt.show()
+
+'''
+Cuando x sea 0 (el ingreso sea 0), se va a consumir 2.88. 
+Por cada unidad que aumente el ingreso, el consumo aumenta en 0.34 unidades.
+'''
